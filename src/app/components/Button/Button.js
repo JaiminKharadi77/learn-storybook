6d417e54d6857873c8b3fdb025bb467780f3b71a
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-09 09:33:08 +0530

    React Storybook Tutorial - 3 - Writing Stories

---
+++ src/app/components/Button/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Button(props) {
+  const { variant = "primary", children, ...rest } = props;
+
+  const colors = {
+    primary: "bg-blue-500",
+    secondary: "bg-gray-300 !text-black",
+    success: "bg-green-500",
+    danger: "bg-red-500",
+  };
+
+  const color = colors[variant] || "bg-gray-500"; // Default fallback
+
+  return (
+    <button
+      className={`px-8 py-4 text-white text-lg rounded-[30px] cursor-pointer ${color}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Button from "./components/Button/Button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h2 className="bg-black text-white">Jaimin Kharadi</h2>
+      <Button variant="primary">Lets</Button>
     </div>
   );
 }
