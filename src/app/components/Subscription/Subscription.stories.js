022a20c82a0f725112fc8a0fb37fd292aa3a3d39
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-12 21:05:25 +0530

    React Storybook Tutorial - 4.3 - Story within Story

---
+++ src/app/components/Subscription/Subscription.stories.js
@@ -0,0 +1,15 @@
+import { Large } from "../Input/Input.stories";
+import { Primary } from "../Button/Button.stories";
+
+export default {
+  title: "form/Subscription",
+};
+
+export const PrimarySubscription = () => {
+  return (
+    <>
+      <Large />
+      <Primary />
+    </>
+  );
+};
