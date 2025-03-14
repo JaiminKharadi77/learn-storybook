f3fa912b70e5fc78aef1295e5cfa2cddf06cd38a
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-15 01:16:56 +0530

    React Storybook Tutorial - 4.4 - Using args in V6
    -By using chatgpt i was able to toggle the Subscription component

--- src/app/components/Subscription/Subscription.stories.js
+++ src/app/components/Subscription/Subscription.stories.js
@@ -1,15 +1,39 @@
-import { Large } from "../Input/Input.stories";
-import { Primary } from "../Button/Button.stories";
+import React from "react";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
 export default {
   title: "form/Subscription",
+  component: Input, // Register component for controls
+  argTypes: {
+    size: {
+      control: "select",
+      options: ["small", "medium", "large"],
+    },
+    placeholder: {
+      control: "text",
+    },
+    variant: {
+      control: "select",
+      options: ["primary", "secondary", "success", "danger"],
+    },
+    children: {
+      control: "text",
+    },
+  },
 };
 
-export const PrimarySubscription = () => {
-  return (
-    <>
-      <Large />
-      <Primary />
-    </>
-  );
+const Template = (args) => (
+  <>
+    <Input size={args.size} placeholder={args.placeholder} />
+    <Button variant={args.variant}>{args.children}</Button>
+  </>
+);
+
+export const PrimarySubscription = Template.bind({});
+PrimarySubscription.args = {
+  size: "medium",
+  placeholder: "Enter email",
+  variant: "primary",
+  children: "Subscribe",
 };
